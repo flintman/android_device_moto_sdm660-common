45ff41310c4195ca2b4e66904fb733f177609d0d
William Bellavnce <flintman@flintmancomputers.com>
2020-08-20 05:17:56 -0400

    Convert from MotoActions --> DirtyUnicorns
    Change-Id: I46ec1be0e0a6d581e0d1446ddeea377f01c00ea4

--- MotoActions/src/com/moto/actions/MotoActionsSettings.java
+++ MotoActions/src/com/moto/actions/MotoActionsSettings.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.moto.actions;
+package com.dirtyunicorns.settings.device;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -23,9 +23,9 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.UserHandle;
 import android.preference.PreferenceManager;
 
-import com.moto.actions.actions.UpdatedStateNotifier;
-import com.moto.actions.actions.CameraActivationAction;
-import com.moto.actions.actions.TorchAction;
+import com.dirtyunicorns.settings.device.actions.UpdatedStateNotifier;
+import com.dirtyunicorns.settings.device.actions.CameraActivationAction;
+import com.dirtyunicorns.settings.device.actions.TorchAction;
 
 public class MotoActionsSettings {
     private static final String TAG = "MotoActions";
