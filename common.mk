ecd604ce41024cd8b49e66e6ed27b9059d7bed3b
William Bellavance <flintman@flintmancomputers.com>
2021-07-07 10:47:13 -0400

    Revert "sdm660-common: Add protobuf vendorcompat lib"
    This reverts commit 2ec6f096dd907fdeec46f6407531071b207bf458.

--- common.mk
+++ common.mk
@@ -299,11 +299,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
