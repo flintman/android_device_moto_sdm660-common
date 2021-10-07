a5f290f8f65f22d502fa820615e72aee2c9a9a41
Arian <arian.kulmer@web.de>
2021-10-07 13:29:32 +0200

    sdm660-common: Add soc properties
    Change-Id: Ib081ffc6e052b0165223c4a53b4d709a9c0a5fab

--- properties.mk
+++ properties.mk
@@ -212,6 +212,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ril.subscription.types=NV,RUIM \
     telephony.lteOnCdmaDevice=1
 
+# SOC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=SDM660
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
