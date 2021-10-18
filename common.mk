342055495a816df1d49c42fb32801465784c98ce
William Bellavance <flintman@flintmancomputers.com>
2021-10-18 07:06:39 -0400

    sdm660-common: tmp drop boot jars
    Change-Id: I0cfe0ea4d7b1f7d642d911881ad0ef1073bea1e9

--- common.mk
+++ common.mk
@@ -373,8 +373,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+ #   telephony-ext
 
 # Tethering
 PRODUCT_PACKAGES += \
